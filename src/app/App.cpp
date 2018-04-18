8f1a99500509f83cc73f0a3fcceed763183f0afb
Casper Skjærris <casper_skj@live.dk>
2018-04-18 13:51:13 +0200

    Changed parser path

--- src/app/App.cpp
+++ src/app/App.cpp
@@ -12,7 +12,7 @@ int Application::App::run() {
     
     try {
         
-        auto parser = new util::Parser("/Users/gronbech/Desktop/Software/c++/SAT/data/cnfs/uf20-91/test.cnf");
+        auto parser = new util::Parser("/Users/casperskjaerris/Documents/DTU/4. Semester/Fagprojekt/SAT/data/cnfs");
         cnf::Formula *f = parser->parse();
         
         algorithms::CDCL *c = new algorithms::CDCL();
