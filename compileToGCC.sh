044334c48da4168f388bdb9b804966b7c5d46702
Magnus Hartvig Grønbech <groenbech96@gmail.com>
2018-05-29 21:33:59 +0200

    path

--- compileToGCC.sh
+++ compileToGCC.sh
@@ -8,4 +8,5 @@ cp src/**/*.* gcc
 cp src/*.* gcc
 cp -R lib gcc
 
-gcc gcc/*.cpp -std=c++17 -I lib -I /usr/local/Cellar/boost/1.66.0 -o sat
+#gcc gcc/*.cpp -std=c++17 -I lib -I /usr/local/Cellar/boost/1.66.0 -o sat
+gcc gcc/*.cpp -std=c++17 -I lib -I /usr/local/boost -o sat
